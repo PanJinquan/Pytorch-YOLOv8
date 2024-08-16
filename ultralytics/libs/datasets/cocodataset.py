54bf10092d0ab96298be4c7f539b2127549c4bd5
潘金全 <panjinquan@dm-ai.com>
2024-08-16 10:58:15 +0800

    add datasets

---
+++ ultralytics/libs/datasets/cocodataset.py
@@ -0,0 +1,150 @@
+# -*-coding: utf-8 -*-
+"""
+    @Author : PKing
+    @E-mail :
+    @Date   : 2023-08-15 19:30:41
+    @Brief  :
+"""
+import os
+import numpy as np
+from ultralytics.utils import RANK, colorstr
+from ultralytics.data.dataset import YOLODataset
+from ultralytics.data.utils import HELP_URL, LOGGER
+from pybaseutils import image_utils, file_utils, json_utils, yaml_utils
+from pybaseutils.dataloader import parser_coco_ins, parser_coco_kps
+
+
+def build_coco_dataset(cfg, img_path, batch, data, mode="train", rect=False, stride=32):
+    """Build COCO Dataset."""
+    return COCODataset(
+        img_path=img_path,
+        imgsz=cfg.imgsz,
+        batch_size=batch,
+        augment=mode == "train",  # augmentation
+        hyp=cfg,  # TODO: probably add a get_hyps_from_cfg function
+        rect=cfg.rect or rect,  # rectangular batches
+        cache=cfg.cache or None,
+        single_cls=cfg.single_cls or False,
+        stride=int(stride),
+        pad=0.0 if mode == "train" else 0.5,
+        prefix=colorstr(f"{mode}: "),
+        task=cfg.task,
+        classes=cfg.classes,
+        data=data,
+        fraction=cfg.fraction if mode == "train" else 1.0,
+    )
+
+
+class COCODataset(YOLODataset):
+    """
+    Dataset class for loading object detection and/or segmentation labels in YOLO format.
+
+    Args:
+        data (dict, optional): A dataset YAML dictionary. Defaults to None.
+        use_segments (bool, optional): If True, segmentation masks are used as labels. Defaults to False.
+        use_keypoints (bool, optional): If True, keypoints are used as labels. Defaults to False.
+
+    Returns:
+        (torch.utils.data.Dataset): A PyTorch dataset object that can be used for training an object detection model.
+    """
+    cache_version = '1.0.2'  # dataset labels *.cache version, >= 1.0.0 for YOLOv8
+
+    def __init__(self, *args, data=None, task="detect", **kwargs):
+        self.unique = False
+        self.use_segments = task == "segment"
+        self.use_keypoints = task == "pose"
+        self.use_obb = task == "obb"
+        self.data: dict = data
+        # self.class_dict = {n: i for i, n in data['names'].items()}
+        self.class_dict = self.parser_classes(data['names'])
+        # image_dir = os.path.join(os.path.dirname(kwargs['img_path']), "person")
+        anno_file = kwargs["img_path"]
+        self.check = self.data.get("check", True)
+        if self.use_keypoints:
+            self.data_parser = parser_coco_kps.CocoKeypoints(anno_file,
+                                                             image_dir=None,
+                                                             class_name=self.class_dict,
+                                                             decode=False,
+                                                             check=self.check)
+        else:
+            self.data_parser = parser_coco_ins.CocoInstances(anno_file,
+                                                             image_dir=None,
+                                                             class_name=self.class_dict,
+                                                             decode=False,
+                                                             check=self.check)
+        super().__init__(*args, data=data, task=task, **kwargs)
+
+    def parser_classes(self, names: dict):
+        class_dict = {}
+        for i, ns in names.items():
+            ns = ns.split(",")
+            for n in ns:
+                class_dict[n] = i
+        return class_dict
+
+    def get_img_files(self, *args, **kwargs):
+        """Read image files. 返回空即可"""
+        file_list = []
+        # files_info = self.coco.get_files_info()
+        # for data in files_info:
+        #     file = os.path.join(self.coco.image_dir, data['file_name'])
+        #     file_list.append(file)
+        return file_list
+
+    def get_labels(self):
+        """
+        Returns dictionary of labels for YOLO training.
+        bbox_format:
+               - `xyxy` means left top and right bottom
+               - `xywh` means center x, center y and width, height(yolo format)
+               - `ltwh` means left top and width, height(coco format)
+        """
+        labels = []
+        # for files_info, annos_info in zip(self.coco.files_info, self.coco.annos_info):
+        for i in range(len(self.data_parser.image_ids)):
+            data_info = self.data_parser.__getitem__(i)
+            im_file = data_info["image_file"]
+            w, h = data_info['size']
+            # boxes, cls, mask, segs = self.coco.get_object_instance(annos_info, h, w, decode=False)
+            boxes, cls, mask, segs = data_info['boxes'], data_info['labels'], data_info['mask'], data_info['segs']
+            kpts = data_info.get("keypoints", [])
+            # 将(x,y,x,y)转为(x_center y_center width height)，见ultralytics.utils.instance
+            if self.check and len(boxes) == 0: continue  # 是否要去除无目标的数据，不去除好像也没有报错
+            cxcywh = image_utils.xyxy2cxcywh(boxes) / (w, h, w, h) if len(boxes) > 0 else boxes
+            cxcywh = cxcywh.reshape(-1, 4)
+            cls = cls.reshape(-1, 1)
+            segs = [s[0] for s in segs]
+            if self.use_obb: segs = image_utils.find_minAreaRect(segs)
+            segs = [s / (w, h) for s in segs] if len(segs) > 0 else []
+            kpts = np.asarray(kpts)
+            if len(kpts) > 0 and kpts.shape[2] == 2:
+                ones = np.zeros(shape=(kpts.shape[0], kpts.shape[1], 1)) + 2
+                kpts = np.concatenate((kpts, ones), axis=-1)
+            kpts = [s / (w, h, 1) for s in kpts] if len(kpts) > 0 else []
+            kpts = np.asarray(kpts)  # (1,17,3)
+            item = {
+                "im_file": im_file,
+                "shape": (h, w),
+                "cls": cls,
+                "bboxes": cxcywh,  # (x_center y_center width height)
+                "segments": segs,  # (nums,num-points,2)
+                "keypoints": kpts,  # (1,17,3)
+                "normalized": True,
+                "bbox_format": "xywh",  # YOLO中xywh指的是(x_center y_center width height)
+            }
+            labels.append(item)
+
+        assert len(labels), f'No valid labels found, please check your dataset. {HELP_URL}'
+        self.im_files = [lb['im_file'] for lb in labels]  # update im_files
+
+        # Check if the dataset is all boxes or all segments
+        lengths = ((len(lb['cls']), len(lb['bboxes']), len(lb['segments'])) for lb in labels)
+        len_cls, len_boxes, len_segments = (sum(x) for x in zip(*lengths))
+        if len_segments and len_boxes != len_segments:
+            LOGGER.warning(
+                f'WARNING ⚠️ Box and segment counts should be equal, but got len(segments) = {len_segments}, '
+                f'len(boxes) = {len_boxes}. To resolve this only boxes will be used and all segments will be removed. '
+                'To avoid this please supply either a detect or segment dataset, not a detect-segment mixed dataset.')
+            for lb in labels:
+                lb['segments'] = []
+        return labels
---
+++ ultralytics/libs/datasets/vocdataset.py
@@ -0,0 +1,128 @@
+# -*-coding: utf-8 -*-
+"""
+    @Author : PKing
+    @E-mail : 
+    @Date   : 2023-08-15 19:30:41
+    @Brief  :
+"""
+import os
+from ultralytics.utils import RANK, colorstr
+from ultralytics.data.dataset import YOLODataset
+from ultralytics.data.utils import HELP_URL, LOGGER
+from pybaseutils import image_utils, file_utils, json_utils, yaml_utils
+from pybaseutils.dataloader import parser_voc
+
+
+def build_voc_dataset(cfg, img_path, batch, data, mode="train", rect=False, stride=32):
+    """Build VOC Dataset."""
+    return VOCDataset(
+        img_path=img_path,
+        imgsz=cfg.imgsz,
+        batch_size=batch,
+        augment=mode == "train",  # augmentation
+        hyp=cfg,  # TODO: probably add a get_hyps_from_cfg function
+        rect=cfg.rect or rect,  # rectangular batches
+        cache=cfg.cache or None,
+        single_cls=cfg.single_cls or False,
+        stride=int(stride),
+        pad=0.0 if mode == "train" else 0.5,
+        prefix=colorstr(f"{mode}: "),
+        task=cfg.task,
+        classes=cfg.classes,
+        data=data,
+        fraction=cfg.fraction if mode == "train" else 1.0,
+    )
+
+
+class VOCDataset(YOLODataset):
+    """
+    Dataset class for loading object detection and/or segmentation labels in YOLO format.
+
+    Args:
+        data (dict, optional): A dataset YAML dictionary. Defaults to None.
+        use_segments (bool, optional): If True, segmentation masks are used as labels. Defaults to False.
+        use_keypoints (bool, optional): If True, keypoints are used as labels. Defaults to False.
+
+    Returns:
+        (torch.utils.data.Dataset): A PyTorch dataset object that can be used for training an object detection model.
+    """
+    cache_version = '1.0.2'  # dataset labels *.cache version, >= 1.0.0 for YOLOv8
+
+    def __init__(self, *args, data=None, task="detect", **kwargs):
+        self.unique = False
+        self.use_segments = task == "segment"
+        self.use_keypoints = task == "pose"
+        self.use_obb = task == "obb"
+        self.data: dict = data
+        # self.class_dict = {n: i for i, n in data['names'].items()}
+        self.class_dict = self.parser_classes(data['names'])
+        # image_dir = os.path.join(os.path.dirname(kwargs['img_path']), "person")
+        anno_file = kwargs["img_path"]
+        self.check = self.data.get("check", True)
+        self.data_parser = parser_voc.VOCDatasets(filename=anno_file, image_dir=None, class_name=self.class_dict,
+                                                  check=self.check)
+        super().__init__(*args, data=data, task=task, **kwargs)
+
+    def parser_classes(self, names: dict):
+        class_dict = {}
+        for i, ns in names.items():
+            ns = ns.split(",")
+            for n in ns:
+                class_dict[n] = i
+        return class_dict
+
+    def get_img_files(self, *args, **kwargs):
+        """Read image files. 返回空即可"""
+        file_list = []
+        # for i in range(len(self.voc.image_ids)):
+        #     image_file, annotation_file = self.voc.get_image_anno_file(i)
+        #     file_list.append(image_file)
+        return file_list
+
+    def get_labels(self):
+        """
+        Returns dictionary of labels for YOLO training.
+        bbox_format:
+               - `xyxy` means left top and right bottom
+               - `xywh` means center x, center y and width, height(yolo format)
+               - `ltwh` means left top and width, height(coco format)
+        """
+        labels = []
+        for i in range(len(self.data_parser.image_ids)):
+            data_info = self.data_parser.__getitem__(i)
+            im_file = data_info['image_file']
+            w, h = data_info['size']
+            targets = data_info["target"]
+            if self.check and len(targets) == 0: continue  # 是否要去除无目标的数据，不去除好像也没有报错
+            boxes, cls = targets[:, 0:4], targets[:, 4:5]
+            # 将(x,y,x,y)转为(x_center y_center width height)，见ultralytics.utils.instance
+            cxcywh = image_utils.xyxy2cxcywh(boxes) / (w, h, w, h) if len(boxes) > 0 else boxes
+            cxcywh = cxcywh.reshape(-1, 4)
+            cls = cls.reshape(-1, 1)
+            # segs = [s[0] / (w, h) for s in segs]
+            item = {
+                "im_file": im_file,
+                "shape": (h, w),
+                "cls": cls,
+                "bboxes": cxcywh,
+                "segments": [],
+                "keypoints": None,
+                "normalized": True,
+                "bbox_format": "xywh",  # YOLO中xywh指的是(x_center y_center width height)
+            }
+            labels.append(item)
+
+        assert len(labels), f'No valid labels found, please check your dataset. {HELP_URL}'
+        self.im_files = [lb['im_file'] for lb in labels]  # update im_files
+
+        # Check if the dataset is all boxes or all segments
+        lengths = ((len(lb['cls']), len(lb['bboxes']), len(lb['segments'])) for lb in labels)
+        len_cls, len_boxes, len_segments = (sum(x) for x in zip(*lengths))
+        if len_segments and len_boxes != len_segments:
+            LOGGER.warning(
+                f'WARNING ⚠️ Box and segment counts should be equal, but got len(segments) = {len_segments}, '
+                f'len(boxes) = {len_boxes}. To resolve this only boxes will be used and all segments will be removed. '
+                'To avoid this please supply either a detect or segment dataset, not a detect-segment mixed dataset.')
+            for lb in labels:
+                lb['segments'] = []
+        return labels
